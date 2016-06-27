62f164f354fdf7134298f0b760e88dd74fbe6744
tetafro <tetafro@gmail.com>
2016-06-27 20:05:40 +0300

    Fixed deleting opened tabs.

--- project/public/js/collections/EditorsCollection.js
+++ project/public/js/collections/EditorsCollection.js
@@ -12,6 +12,20 @@ define(
         var EditorsCollection = Backbone.Collection.extend({
             model: Note,
 
+            initialize: function () {
+                this.listenTo(this, 'remove', this.onRemove);
+            },
+
+            // This fires when tab is closed and when model is destroyed
+            onRemove: function (note) {
+                // Make active first of remaining opened notes
+                if (this.length && note.get('active')) {
+                    this.at(0).set('active', true);
+                }
+
+                note.set({active: false, opened: false});
+            },
+
             openOne: function(note) {
                 var eachNote;
                 this.each(function (eachNote) {
@@ -25,15 +39,8 @@ define(
             },
 
             closeOne: function(note) {
-                // Remove from opened and set unactive
+                // Remove from opened
                 this.remove(note);
-
-                // Make active first of remaining opened notes
-                if (this.length && note.get('active')) {
-                    this.at(0).set('active', true);
-                }
-
-                note.set({active: false, opened: false});
             }
         });
 
