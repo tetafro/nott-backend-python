459ab48bf7f74ea3aed2ae51b41a8e176ee82e19
tetafro <tetafro@gmail.com>
2017-05-12 22:12:39 +0300

    Django dev mode for managing static files.

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^ajax/', include('apps.notes.urls_ajax')),
     url(r'', include('apps.notes.urls_web')),
     url(r'', include('apps.users.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
