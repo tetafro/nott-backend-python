427753225c79708911f7447d7cf17db46faf43f5
tetafro <tetafro@gmail.com>
2017-07-03 23:44:46 +0300

    Dropbox upload script changed for using v2 API.

--- scripts/dropbox_upload.py
+++ scripts/dropbox_upload.py
@@ -8,16 +8,17 @@ TODO: get proper Dropbox token from pass.ini
 import dropbox
 import sys, os
 
+TOKEN = 'my-token'
+
 files = sys.argv[1:]
-token = 'my-token'
-client = dropbox.client.DropboxClient(token)
+client = dropbox.Dropbox(TOKEN)
 
 for file in files:
-    f = open(file, 'rb')
-    try:
-        remote_file = os.path.basename(file)
-        response = client.put_file(remote_file, f)
-    except dropbox.rest.ErrorResponse as e:
-        print(str(e))
-    else:
-        print('Uploaded succesfully')
+    with open(file, 'rb') as f:
+        try:
+            filename = os.path.basename(file)
+            response = client.files_upload(f.read(), '/'+   filename)
+        except Exception as e:
+            print('Upload error: '+str(e))
+        else:
+            print('Uploaded succesfully')
