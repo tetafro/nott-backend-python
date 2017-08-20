0e0c809c1ce08bac232c11c5ceee7481f2e5ae67
tetafro <tetafro@gmail.com>
2017-08-20 15:45:20 +0300

    Add custom login decorator for AJAX views on backend.

--- project/apps/notes/urls_ajax.py
+++ project/apps/notes/urls_ajax.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
-from django.contrib.auth.decorators import login_required
+from core.decorators import login_required_ajax
 from .views_ajax import FolderView, NotepadView, NoteView, SearchView
 
 urlpatterns = [
-    url(r'^folders/?$', login_required(FolderView.as_view())),
-    url(r'^folders/(?P<id>\d+)/?$', login_required(FolderView.as_view())),
-    url(r'^notepads/?$', login_required(NotepadView.as_view())),
-    url(r'^notepads/(?P<id>\d+)/?$', login_required(NotepadView.as_view())),
-    url(r'^notes/?$', login_required(NoteView.as_view())),
-    url(r'^notes/(?P<id>\d+)/?$', login_required(NoteView.as_view())),
-    url(r'^search/?$', login_required(SearchView.as_view())),
+    url(r'^folders/?$', login_required_ajax(FolderView.as_view())),
+    url(r'^folders/(?P<id>\d+)/?$', login_required_ajax(FolderView.as_view())),
+    url(r'^notepads/?$', login_required_ajax(NotepadView.as_view())),
+    url(r'^notepads/(?P<id>\d+)/?$', login_required_ajax(NotepadView.as_view())),
+    url(r'^notes/?$', login_required_ajax(NoteView.as_view())),
+    url(r'^notes/(?P<id>\d+)/?$', login_required_ajax(NoteView.as_view())),
+    url(r'^search/?$', login_required_ajax(SearchView.as_view())),
 ]
