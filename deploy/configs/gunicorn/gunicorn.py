dcc59d25c8551b154d334fb5dd531f66b4c46c94
tetafro <tetafro@gmail.com>
2017-10-31 08:50:33 +0300

    Take hosts and domain for Ansible playbook from env.

--- deploy/configs/gunicorn/gunicorn.py
+++ deploy/configs/gunicorn/gunicorn.py
@@ -1,6 +1,6 @@
-proc_name = "nott"
-bind = "0.0.0.0:8000"
-workers = "4"
-loglevel = "info"
-accesslog = "-"
-errorlog = "-"
+proc_name = 'nott'
+bind = '0.0.0.0:8000'
+workers = '4'
+loglevel = 'info'
+accesslog = '-'
+errorlog = '-'
