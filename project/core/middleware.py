74ba8fee53394b873cfadf8ea9f235a28acf92f7
tetafro <tetafro@gmail.com>
2017-11-03 08:18:26 +0300

    Add logs verbosity for production env.

--- project/core/middleware.py
+++ project/core/middleware.py
@@ -1,4 +1,10 @@
+import logging
+
 from django.shortcuts import render
+
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 
 # Classes for HTTP errors
@@ -20,10 +26,13 @@ class HttpErrorsMiddleware(object):
     """
     def process_exception(self, request, exception):
         if isinstance(exception, Http400):
+            logger.exception('Bad request error')
             return render(request, '400.html', status=400)
         elif isinstance(exception, Http403):
+            logger.exception('Forbidden error')
             return render(request, '403.html', status=403)
         elif isinstance(exception, Http500):
+            logger.exception('Internal server error')
             return render(request, '500.html', status=500)
         else:
             return None
