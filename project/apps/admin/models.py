7715c9aaf848010a8986d98ffef4bd734db4b97d
tetafro <tetafro@gmail.com>
2017-12-04 08:12:46 +0300

    Make code field unique in the settings table.

--- project/apps/admin/models.py
+++ project/apps/admin/models.py
@@ -6,7 +6,7 @@ class Config(models.Model):
     Application-wide settings that can be set by admins
     """
 
-    code = models.CharField(max_length=20)
+    code = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=80)
     datatype = models.CharField(max_length=20)
     value = models.CharField(max_length=80)
