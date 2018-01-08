eca1c1e56e5d42164f2e0a1ece69144b6ecc568c
tetafro <tetafro@gmail.com>
2018-01-08 18:11:42 +0300

    Terminate non-existing API endpoints.

--- project/apps/base/views.py
+++ project/apps/base/views.py
@@ -1,6 +1,13 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
 def index(request):
     """HTML frame for JS app"""
+
+    # Terminate non-existing API endpoints
+    if request.get_full_path()[:4] == '/api':
+        return JsonResponse({'error': 'not found'}, status=404)
+
+    # Render the same page on any non-API request
     return render(request, 'index.html')
