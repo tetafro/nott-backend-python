c471e80e5d15469f639707b8ea729e699e0b8e67
tetafro <tetafro@gmail.com>
2018-01-13 21:41:45 +0300

    Healthz should work only with GET requests.

--- project/apps/health/views.py
+++ project/apps/health/views.py
@@ -1,6 +1,9 @@
+from django.http import JsonResponse
 from django.shortcuts import HttpResponse
 
 
 def healthz(request):
     """Endpoint for service healthcheck"""
+    if request.method != 'GET':
+        JsonResponse({'error': 'method not allowed'}, status=405)
     return HttpResponse('', status=200)
